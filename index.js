6e8a6dac24b9e267b176fce43db870f46a188986
mnapearson <marratoon@gmail.com>
2020-10-09 11:38:30 +0200

    Add slider

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+})
