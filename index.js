6dd7cdb890b97ca4f8cf1180bbe90f1203bfa4dc
mnapearson <marratoon@gmail.com>
2020-10-13 18:30:11 +0200

    Final style edits

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-var mySwiper = new Swiper('.swiper-container', {
+const mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
-  direction: 'horizontal',
-  loop: false,
+  direction: "horizontal",
+  loop: true,
 
   breakpoints: {
     // when window width is >= 320px
-    375: {
+    360: {
       slidesPerView: 2,
-     
-  
     },
     // when window width is >= 480px
     520: {
       slidesPerView: 3,
-
     },
     // when window width is >= 640px
     680: {
       slidesPerView: 4,
-  
     },
 
     960: {
       slidesPerView: 5,
-  
-    }
+    },
   },
- 
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-})
+});
 
+const url =
+  "https://www.spreadshirt.com/shopData/list?query=K118614&locale=us_US";
