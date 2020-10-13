190568333876f217c503cefb376e2d8b4f66144e
mnapearson <marratoon@gmail.com>
2020-10-13 11:59:50 +0200

    Final edit for full desktop view

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ var mySwiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
   loop: false,
   slidesPerView: 5,
+
+  breakpoints: {
+    // when window width is >= 320px
+    375: {
+      slidesPerView: 2,
+     
+  
+    },
+    // when window width is >= 480px
+    520: {
+      slidesPerView: 3,
+
+    },
+    // when window width is >= 640px
+    680: {
+      slidesPerView: 4,
+  
+    },
+
+    960: {
+      slidesPerView: 5,
+  
+    }
+  },
  
 
   // Navigation arrows
