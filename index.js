fb95a67ffbb29c5a3840de31280af4aaea70b7a6
mnapearson <marratoon@gmail.com>
2020-10-14 17:11:36 +0200

    Final product

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
 
   breakpoints: {
-    // when window width is >= 320px
-    360: {
+    350: {
       slidesPerView: 2,
     },
-    // when window width is >= 480px
+
     520: {
       slidesPerView: 3,
     },
-    // when window width is >= 640px
+
     680: {
       slidesPerView: 4,
     },
@@ -22,7 +20,6 @@ const mySwiper = new Swiper(".swiper-container", {
     },
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -31,3 +28,32 @@ const mySwiper = new Swiper(".swiper-container", {
 
 const url =
   "https://www.spreadshirt.com/shopData/list?query=K118614&locale=us_US";
+
+const swiperWrapper = document.getElementById("swiper-wrapper");
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.articles.forEach((article) => {
+      const slide = document.createElement("div");
+      slide.className = "swiper-slide";
+      const productId = article.productId;
+      const productURL =
+        "https://image.spreadshirtmedia.com/image-server/v1/mp/products/" +
+        productId +
+        "/views/1,width=378,height=378,backgroundColor=F2F2F2";
+      const img = document.createElement("img");
+      img.src = productURL;
+      const h1 = document.createElement("h1");
+      h1.textContent = article.name;
+      const p = document.createElement("p");
+      p.textContent = "$" + article.price;
+
+      slide.appendChild(img);
+      slide.appendChild(h1);
+      slide.appendChild(p);
+      swiperWrapper.appendChild(slide);
+    });
+  });
